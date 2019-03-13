cb95059aa5604a187fa7cc3cc3adf98764c109f7
wmarc04 <wmarc-antoine@hotmail.com>
2019-03-13 13:01:27 -0400

    Trip component

--- app/components/screens/trips/TripDetails.js
+++ app/components/screens/trips/TripDetails.js
@@ -1,35 +1,43 @@
 import React, { Component } from 'react'
-import { StyleSheet, View } from 'react-native';
-import { connect } from 'react-redux';
-import { Container, Text, Card, CardItem, Body, Icon, H1, H2, H3 } from 'native-base';
-import { ScrollView } from 'react-native-gesture-handler';
-import EcovoMapView from '../../astuvu-native/EcovoMapView';
+import { StyleSheet, View } from 'react-native'
+import { connect } from 'react-redux'
+import { Container, Card } from 'native-base'
 import { getTripList } from '../../../actions/trip'
+import EcovoMapView from '../../astuvu-native/EcovoMapView'
+import TripCarDetails from './TripCarDetails'
+import TripStopDetails from './TripStopDetails'
 
 var markers = [
     {
         latitude: 45.5016889,
         longitude: -73.567256,
         title: 'Origin',
-        subtitle: 'Montreal'
+        subtitle: 'Montreal',
+        hour: "14h00",
+        duration: "1h30"
     },
     {
         latitude: 45.404476,
         longitude: -71.888351,
         title: 'Stop 1',
-        subtitle: 'Sherbrook'
+        subtitle: 'Sherbrook',
+        hour: "15h30",
+        duration: "3h00"
     },
     {
         latitude: 46.3938,
         longitude: -72.6534,
         title: 'Stop 2',
-        subtitle: 'Trois-Rivière'
+        subtitle: 'Trois-Rivière',
+        hour: "18h30",
+        duration: "1h00"
     },
     {
         latitude: 46.8138,
         longitude: -71.2079,
         title: 'Destination',
-        subtitle: 'Quebec'
+        subtitle: 'Quebec',
+        hour: "19h30"
     },
 ];
 
@@ -63,81 +71,11 @@ class TripDetails extends Component {
     render() {
         return (
             <Container style={styles.container}>
-
                 <EcovoMapView markers={markers} origin={origin} destination={destination} stopPoints={stopPoint} />
                 <View style={styles.bottom}>
-                    <Card style={{height: '45%', paddingBottom: 60}}>
-                        <CardItem header>
-                            <Icon type="MaterialIcons" name="directions-car" />
-                            <View style={{ flex: 1 }}>
-                                <View style={styles.textSameLine}>
-                                    <Text>Toyota Camry</Text>
-                                    <Text style={styles.textLight}> 2016</Text>
-                                </View>
-                                <Text style={styles.textLight}>Green</Text>
-                            </View>
-                        </CardItem>
-                        <CardItem>
-                            <Body>
-                                <ScrollView style={{ width: '100%'}}>
-                                    <View style={{ flex: 1, flexDirection: 'row', width: '100%' }}>
-                                        <View style={{ paddingHorizontal: 25  }}>
-                                            <Icon type="FontAwesome5" name="circle" />
-
-                                            <Icon type="SimpleLineIcons" name="options-vertical" style={{resizeMode: 'repeat', height: '82%'}} />
-
-                                            <Icon type="FontAwesome5" name="circle" />
-                                        </View>
-                                        <View style={{ paddingHorizontal: 25  }}>
-                                            <View>
-                                                <View style={styles.textSameLine}>
-                                                    <H1>Start</H1>
-                                                    <Text>- 14:00</Text>
-                                                </View>
-                                                <Text>Montréal</Text>
-                                            </View>
-
-                                            <View>
-                                                <Text style={styles.textBold}>1h30</Text>
-                                            </View>
-
-                                            <View>
-                                                <View style={styles.textSameLine}>
-                                                    <H2>Stop 1</H2>
-                                                    <Text>- 15:30</Text>
-                                                </View>
-                                                <Text>Sherbrook</Text>
-                                            </View>
-
-                                            <View>
-                                                <Text style={styles.textBold}>3h00</Text>
-                                            </View>
-
-                                            <View>
-                                                <View style={styles.textSameLine}>
-                                                    <H2>Stop 2</H2>
-                                                    <Text>- 18:30</Text>
-                                                </View>
-                                                <Text>Trois-Rivière</Text>
-                                            </View>
-
-                                            <View>
-                                                <Text style={styles.textBold}>1h00</Text>
-                                            </View>
-
-                                            <View>
-                                                <View style={styles.textSameLine}>
-                                                    <H1>Finish</H1>
-                                                    <Text>- 19:30</Text>
-                                                </View>
-                                                <Text>Québec</Text>
-                                            </View>
-
-                                        </View>
-                                    </View>
-                                </ScrollView>
-                            </Body>
-                        </CardItem>
+                    <Card style={styles.cardStyle}>
+                        <TripCarDetails carMake="Toyota" carModel="Camry" carYear="2016" carColor="Green" />
+                        <TripStopDetails stops={markers} />
                     </Card>
                 </View>
             </Container>
@@ -150,10 +88,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
     },
-    map: {
-        ...StyleSheet.absoluteFillObject,
-
-    },
     bottom: {
         flex: 1,
         position: 'absolute',
@@ -164,15 +98,9 @@ const styles = StyleSheet.create({
         paddingBottom: 0,
         height: '65%'
     },
-    textSameLine: {
-        flexDirection: 'row'
-    },
-    textLight: {
-        fontWeight: 'normal'
-    },
-    textBold: {
-        fontWeight: 'bold',
-        paddingVertical: 20
+    cardStyle: {
+        height: '45%',
+        paddingBottom: 60
     }
 })
 
