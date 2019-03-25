fc789703a4b5a6314d83dd312cf2b98a13168272
samrm111 <samuelrondeaumillaire@gmail.com>
2019-03-25 12:39:08 -0400

    Complete footer button

--- app/components/astuvu-native/form/FooterButton.js
+++ app/components/astuvu-native/form/FooterButton.js
@@ -1,41 +1,29 @@
 import React, { Component } from 'react'
 import { StyleSheet, View } from 'react-native'
-import { Button, Text, StyleProvider, Spinner } from 'native-base'
+import { Button, Text, StyleProvider, Spinner, Footer } from 'native-base'
 import PropTypes from 'prop-types'
 import getTheme from '../native-base-theme/components'
 import commonColors from '../native-base-theme/variables/commonColor'
-
-class SubmitButton extends Component {
+import SubmitButton from './SubmitButton'
+class FooterButton extends Component {
     constructor(props) {
         super(props)
-    }
-    _renderText = () => {
-        if (this.props.loading) {
-            return <Spinner color='white'/>
-
-        } else {
-            return <Text>{this.props.text}</Text>
-        }
     }
 
     render = () => {
         return (
             <StyleProvider style={getTheme(commonColors)}>
-                <Button style={{ ...this.props.style }} transparent={this.props.transparent} disabled={this.props.disabled || this.props.formError || this.props.loading } danger={this.props.formError}>
-                    {this._renderText()}
-                </Button>
+                <Footer>
+                    <SubmitButton transparent={false} formError={this.props.formError} text={this.props.text} loading={this.props.loading} disabled={this.props.disabled} style={{ borderRadius:0, ...this.props.style }}></SubmitButton>
+                </Footer>
             </StyleProvider>
         )
     }
 }
 
-export const FieldStyles = StyleSheet.create({
-})
-
 export const FieldPropTypes = {
     style: PropTypes.object,
     text: PropTypes.string,
-    transparent: PropTypes.boolean,
     disabled: PropTypes.boolean,
     loading: PropTypes.boolean,
     formError: PropTypes.boolean,
@@ -44,10 +32,9 @@ export const FieldPropTypes = {
 export const FieldDefaultProps = {
     style: {},
     text: 'Submit',
-    transparent: false,
     disabled: false,
     loading: false,
     formError: false,
 }
 
-export default SubmitButton
+export default FooterButton
